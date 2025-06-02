bd0bd4c5f8c25c24d9ba4ea1a97bcce209003474
Elegy <270663716@qq.com>
2025-06-02 18:03:46 +0800

    feat: add constant

--- src/const/base.ts
+++ src/const/base.ts
@@ -1,2 +1,7 @@
+import { appEnv } from '@/config/app';
+
+export const LOADING_FLAT = '...';
 export const BRANDING_NAME = 'LobeChat';
 export const DEFAULT_USER_AVATAR_URL = 'https://lobehub.com/_next/static/media/logo.98482105.png';
+export const withBasePath = (path: string) => appEnv.NEXT_PUBLIC_BASE_PATH + path;
+export const imageUrl = (filename: string) => withBasePath(`/images/${filename}`);
