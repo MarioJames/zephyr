979360e0be125855c845a9f16ce631908e0b123e
Elegy <270663716@qq.com>
2025-06-02 21:30:27 +0800

    feat: add version.ts

--- src/const/version.ts
+++ src/const/version.ts
@@ -1,1 +1,3 @@
 export const isServerMode = process.env.NEXT_PUBLIC_SERVICE_MODE === 'server';
+export const isUsePgliteDB = process.env.NEXT_PUBLIC_CLIENT_DB === 'pglite';
+export const isDeprecatedEdition = !isServerMode && !isUsePgliteDB;
