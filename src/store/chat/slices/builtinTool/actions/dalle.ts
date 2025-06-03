704356146e23673c655b52253cdf0cbafc48c7b6
Elegy <270663716@qq.com>
2025-06-03 22:11:16 +0800

    feat: add chat store

---
+++ src/store/chat/slices/builtinTool/actions/dalle.ts
@@ -0,0 +1,74 @@
+import { produce } from 'immer';
+import { SWRResponse } from 'swr';
+import { StateCreator } from 'zustand/vanilla';
+
+import { useClientDataSWR } from '@/libs/swr';
+import { fileService } from '@/services/file';
+import { chatSelectors } from '@/store/chat/selectors';
+import { ChatStore } from '@/store/chat/store';
+import { DallEImageItem } from '@/types/tool/dalle';
+import { setNamespace } from '@/utils/storeDebug';
+
+const n = setNamespace('tool');
+
+const SWR_FETCH_KEY = 'FetchImageItem';
+
+export interface ChatDallEAction {
+  generateImageFromPrompts: (items: DallEImageItem[], id: string) => Promise<void>;
+  text2image: (id: string, data: DallEImageItem[]) => Promise<void>;
+  toggleDallEImageLoading: (key: string, value: boolean) => void;
+  updateImageItem: (id: string, updater: (data: DallEImageItem[]) => void) => Promise<void>;
+  useFetchDalleImageItem: (id: string) => SWRResponse;
+}
+
+export const dalleSlice: StateCreator<
+  ChatStore,
+  [['zustand/devtools', never]],
+  [],
+  ChatDallEAction
+> = (set, get) => ({
+  generateImageFromPrompts: async (items, messageId) => {
+    console.log(items)
+  },
+  text2image: async (id, data) => {
+    // const isAutoGen = settingsSelectors.isDalleAutoGenerating(useGlobalStore.getState());
+    // if (!isAutoGen) return;
+
+    await get().generateImageFromPrompts(data, id);
+  },
+
+  toggleDallEImageLoading: (key, value) => {
+    set(
+      { dalleImageLoading: { ...get().dalleImageLoading, [key]: value } },
+      false,
+      n('toggleDallEImageLoading'),
+    );
+  },
+
+  updateImageItem: async (id, updater) => {
+    const message = chatSelectors.getMessageById(id)(get());
+    if (!message) return;
+
+    const data: DallEImageItem[] = JSON.parse(message.content);
+
+    const nextContent = produce(data, updater);
+    await get().internal_updateMessageContent(id, JSON.stringify(nextContent));
+  },
+
+  useFetchDalleImageItem: (id) =>
+    useClientDataSWR([SWR_FETCH_KEY, id], async () => {
+      const item = await fileService.getFile(id);
+
+      set(
+        produce((draft) => {
+          if (draft.dalleImageMap[id]) return;
+
+          draft.dalleImageMap[id] = item;
+        }),
+        false,
+        n('useFetchFile'),
+      );
+
+      return item;
+    }),
+});
