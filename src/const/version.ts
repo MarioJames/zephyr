fad40a7153461c87c287e37b857c7ea2b9cc64b4
Elegy <270663716@qq.com>
2025-06-03 23:39:24 +0800

    feat: add some constant

--- src/const/version.ts
+++ src/const/version.ts
@@ -1,3 +1,6 @@
+import pkg from '@/../package.json';
 export const isServerMode = process.env.NEXT_PUBLIC_SERVICE_MODE === 'server';
 export const isUsePgliteDB = process.env.NEXT_PUBLIC_CLIENT_DB === 'pglite';
 export const isDeprecatedEdition = !isServerMode && !isUsePgliteDB;
+
+export const CURRENT_VERSION = pkg.version;
