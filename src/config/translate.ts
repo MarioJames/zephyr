6f1c179552c7d3df277988f7d20b51ceec561524
Elegy <270663716@qq.com>
2025-06-04 22:08:36 +0800

    feat: translate config

--- src/config/translate.ts
+++ src/config/translate.ts
@@ -1,10 +1,14 @@
 export const localeOptions = [
-        {
-    label: '简体中文',
-    value: 'zh-CN',
+  {
+    label: "简体中文",
+    value: "zh-CN",
   },
   {
-    label: '한국어',
-    value: 'ko-KR',
+    label: "한국어",
+    value: "ko-KR",
   },
-]
+];
+
+export const locales = ["ko-KR", "zh-CN"] as const;
+
+export type Locales = (typeof locales)[number];
