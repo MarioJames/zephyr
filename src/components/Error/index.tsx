d49cacad79ce002b72ed73eaea9c620c92694633
Elegy <270663716@qq.com>
2025-06-05 20:55:45 +0800

    feat: add Error componets

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { Button, FluentEmoji } from '@lobehub/ui';
+import Link from 'next/link';
+import { memo } from 'react';
+import { Flexbox } from 'react-layout-kit';
+
+import { MAX_WIDTH } from '@/const/layoutTokens';
+
+interface ErrorCaptureProps {
+  reset: () => void;
+}
+
+const ErrorCapture = memo<ErrorCaptureProps>(({ reset }) => {
+  return (
+    <Flexbox align={'center'} justify={'center'} style={{ minHeight: '100%', width: '100%' }}>
+      <h1
+        style={{
+          filter: 'blur(8px)',
+          fontSize: `min(${MAX_WIDTH / 6}px, 25vw)`,
+          fontWeight: 900,
+          margin: 0,
+          opacity: 0.12,
+          position: 'absolute',
+          zIndex: 0,
+        }}
+      >
+        ERROR
+      </h1>
+      <FluentEmoji emoji={'🤧'} size={64} />
+      <h2 style={{ fontWeight: 'bold', marginTop: '1em', textAlign: 'center' }}>
+        {'页面遇到一点问题..'}
+      </h2>
+      <p style={{ marginBottom: '2em' }}>{'待会来试试，或者回到已知的世界'}</p>
+      <Flexbox gap={12} horizontal style={{ marginBottom: '1em' }}>
+        <Button onClick={() => reset()}>重新加载</Button>
+        <Link href="/">
+          <Button type={'primary'}>返回首页</Button>
+        </Link>
+      </Flexbox>
+    </Flexbox>
+  );
+});
+
+ErrorCapture.displayName = 'ErrorCapture';
+
+export default ErrorCapture;
