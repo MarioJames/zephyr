4108a85b41471bfd12f131c4cc123a7e6f819e71
Elegy <270663716@qq.com>
2025-06-07 16:36:09 +0800

    feat: add thread service

---
+++ src/services/thread/index.ts
@@ -0,0 +1,7 @@
+import { ClientService } from './client';
+import { ServerService } from './server';
+
+export const threadService =
+  process.env.NEXT_PUBLIC_SERVICE_MODE === 'server'
+    ? new ServerService()
+    : new ClientService();
