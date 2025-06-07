99b44c4d83be56d520d9f9f2523624fecb333a19
Elegy <270663716@qq.com>
2025-06-07 14:33:18 +0800

    feat: add some config

--- src/config/translate.ts
+++ src/config/translate.ts
@@ -8,7 +8,14 @@ export const localeOptions = [
     value: "ko-KR",
   },
 ];
+export const DEFAULT_LANG = "zh-CN";
 
 export const locales = ["ko-KR", "zh-CN"] as const;
 
 export type Locales = (typeof locales)[number];
+
+/**
+ * Check if the language is supported
+ * @param locale
+ */
+export const isLocaleNotSupport = (locale: string) => !locales.includes(locale as Locales);
