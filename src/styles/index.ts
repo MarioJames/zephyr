f87c14938ccbd72230ce1eadfce0394605916c4d
Elegy <270663716@qq.com>
2025-06-14 17:39:22 +0800

    feat: 新增css reset

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,10 +2,12 @@ import { createGlobalStyle } from 'antd-style';
 
 import antdOverride from './antdOverride';
 import global from './global';
+import reset from './reset';
 
 const prefixCls = 'ant';
 
 export const GlobalStyle = createGlobalStyle(({ theme }) => [
+  reset(),
   global({ prefixCls, token: theme }),
   antdOverride({ prefixCls, token: theme }),
 ]);
