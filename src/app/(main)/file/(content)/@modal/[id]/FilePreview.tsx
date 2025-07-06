56fd66c1d695bfd46b1f93350b06b3bcee5ee7b6
Elegy <270663716@qq.com>
2025-07-06 19:54:34 +0800

    feat: 补充兜底展示

--- src/app/(main)/file/(content)/@modal/[id]/FilePreview.tsx
+++ src/app/(main)/file/(content)/@modal/[id]/FilePreview.tsx
@@ -1,28 +1,33 @@
-'use client';
+"use client";
 
-import { Image } from 'antd';
-import { memo } from 'react';
-import { Center } from 'react-layout-kit';
+import { Image } from "antd";
+import { memo } from "react";
+import { useTheme } from "antd-style";
+import { Center } from "react-layout-kit";
 
-import { FILE_URL } from '@/const/base';
-import { fileCoreSelectors, useFileStore } from '@/store/file';
+import { FILE_URL } from "@/const/base";
+import { fileCoreSelectors, useFileStore } from "@/store/file";
 
 const FilePreview = memo<{ id: string }>(({ id }) => {
   const file = useFileStore(fileCoreSelectors.getFileById(id));
-
+  const theme = useTheme();
   if (!file) return null;
 
   // 只支持图片预览
-  if (!file.fileType?.startsWith('image/')) {
+  if (!file.fileType?.startsWith("image/")) {
     return (
-      <Center height={'100%'}>
-        <div>此文件格式暂不支持在线预览</div>
+      <Center height={"100%"}>
+        <div
+          style={{ color: theme.colorText, fontSize: "22px", fontWeight: 500 }}
+        >
+          此文件格式暂不支持在线预览
+        </div>
       </Center>
     );
   }
 
   return (
-    <Center height={'100%'}>
+    <Center height={"100%"}>
       <Image src={`${FILE_URL}${file?.url}`} alt={file?.filename} />
     </Center>
   );
