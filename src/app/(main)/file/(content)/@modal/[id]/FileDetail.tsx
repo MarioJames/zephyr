9de4d5704b03ddef81c85e27314da8bd75504c98
Elegy <270663716@qq.com>
2025-07-06 19:47:55 +0800

    feat: 优化loading

--- src/app/(main)/file/(content)/@modal/[id]/FileDetail.tsx
+++ src/app/(main)/file/(content)/@modal/[id]/FileDetail.tsx
@@ -1,24 +1,51 @@
-'use client';
+"use client";
 
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useState } from "react";
+import { Spin } from "antd";
 
-import { fileCoreSelectors, useFileStore } from '@/store/file';
-import { filesAPI } from '@/services';
+import { fileCoreSelectors, useFileStore } from "@/store/file";
+import { filesAPI } from "@/services";
 
-import Detail from '../../../features/FileDetail';
+import Detail from "../../../features/FileDetail";
 
 const FileDetail = memo<{ id: string }>(({ id }) => {
   const file = useFileStore(fileCoreSelectors.getFileById(id));
   const setFiles = useFileStore((state) => state.setFiles);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     // 如果没有文件数据，则获取文件详情
     if (!file) {
-      filesAPI.getFileDetail(id).then((fileData) => {
-        setFiles([fileData]);
-      });
+      setIsLoading(true);
+      filesAPI
+        .getFileDetail(id)
+        .then((fileData) => {
+          setFiles([fileData]);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
   }, [file, id, setFiles]);
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: "8px",
+        }}
+      >
+        <Spin />
+        <div>数据加载中...</div>
+      </div>
+    );
+  }
 
   if (!file) return null;
 
