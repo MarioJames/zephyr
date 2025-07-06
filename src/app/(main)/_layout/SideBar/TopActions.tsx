8a168c42efdb5bd74f0eec5a12b9e507b35d8e15
Elegy <270663716@qq.com>
2025-07-06 12:55:47 +0800

    feat: tab权限控制

--- src/app/(main)/_layout/SideBar/TopActions.tsx
+++ src/app/(main)/_layout/SideBar/TopActions.tsx
@@ -1,11 +1,18 @@
-import { ActionIcon, ActionIconProps } from '@lobehub/ui';
-import { UsersRound, BookUser, MessageSquare, FolderClosed } from 'lucide-react';
-import Link from 'next/link';
-import { memo } from 'react';
-import { Flexbox } from 'react-layout-kit';
-import { SidebarTabKey } from '@/store/global/initialState';
+import { ActionIcon, ActionIconProps } from "@lobehub/ui";
+import {
+  UsersRound,
+  BookUser,
+  MessageSquare,
+  FolderClosed,
+} from "lucide-react";
+import Link from "next/link";
+import { memo } from "react";
+import { Flexbox } from "react-layout-kit";
+import { SidebarTabKey } from "@/store/global/initialState";
+import { useOIDCStore } from "@/store/oidc";
+import { oidcSelectors } from "@/store/oidc/selectors";
 
-const ICON_SIZE: ActionIconProps['size'] = {
+const ICON_SIZE: ActionIconProps["size"] = {
   blockSize: 40,
   size: 24,
   strokeWidth: 2,
@@ -20,43 +27,48 @@ const TopActions = memo<TopActionProps>(({ tab }) => {
   const isFileActive = tab === SidebarTabKey.File;
   const isCustomerManagementActive = tab === SidebarTabKey.CustomerManagement;
   const isEmployeeManagementActive = tab === SidebarTabKey.EmployeeManagement;
+  const isAdmin = useOIDCStore(oidcSelectors.isCurrentUserAdmin);
 
   return (
     <Flexbox gap={8}>
-      <Link aria-label={"会话"} href={'/chat'}>
+      <Link aria-label={"会话"} href={"/chat"}>
         <ActionIcon
           active={isChatActive}
           icon={MessageSquare}
           size={ICON_SIZE}
           title={"会话"}
-          tooltipProps={{ placement: 'right' }}
+          tooltipProps={{ placement: "right" }}
         />
       </Link>
-      <Link aria-label={"客户管理"} href={'/customer'}>
-        <ActionIcon
-          active={isCustomerManagementActive}
-          icon={BookUser}
-          size={ICON_SIZE}
-          title={"客户管理"}
-          tooltipProps={{ placement: 'right' }}
-        />
-      </Link>
-      <Link aria-label={"员工管理"} href={'/employee'}>
-        <ActionIcon
-          active={isEmployeeManagementActive}
-          icon={UsersRound}
-          size={ICON_SIZE}
-          title={"员工管理"}
-          tooltipProps={{ placement: 'right' }}
-        />
-      </Link>
-      <Link aria-label={"文件管理"} href={'/file'}>
+      {isAdmin && (
+        <Link aria-label={"客户管理"} href={"/customer"}>
+          <ActionIcon
+            active={isCustomerManagementActive}
+            icon={BookUser}
+            size={ICON_SIZE}
+            title={"客户管理"}
+            tooltipProps={{ placement: "right" }}
+          />
+        </Link>
+      )}
+      {isAdmin && (
+        <Link aria-label={"员工管理"} href={"/employee"}>
+          <ActionIcon
+            active={isEmployeeManagementActive}
+            icon={UsersRound}
+            size={ICON_SIZE}
+            title={"员工管理"}
+            tooltipProps={{ placement: "right" }}
+          />
+        </Link>
+      )}
+      <Link aria-label={"文件管理"} href={"/file"}>
         <ActionIcon
           active={isFileActive}
           icon={FolderClosed}
           size={ICON_SIZE}
           title={"文件管理"}
-          tooltipProps={{ placement: 'right' }}
+          tooltipProps={{ placement: "right" }}
         />
       </Link>
     </Flexbox>
