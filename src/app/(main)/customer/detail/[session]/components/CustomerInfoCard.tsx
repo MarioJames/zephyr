928aa02ba3aeaca2a7c2e1a8f3ed8c230ffc54df
Elegy <270663716@qq.com>
2025-07-09 23:50:58 +0800

    style: 优化InfoCard样式

--- src/app/(main)/customer/detail/[session]/components/CustomerInfoCard.tsx
+++ src/app/(main)/customer/detail/[session]/components/CustomerInfoCard.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
-import { Card, Row, Col, Avatar } from 'antd';
-import { createStyles } from 'antd-style';
-import { CustomerItem } from '@/services/customer';
-import { EmployeeAssignmentPanel } from './EmployeeAssignmentPanel';
-import { UserItem } from '@/services/user';
+import React from "react";
+import { Card, Row, Col, Avatar, Tag } from "antd";
+import { createStyles } from "antd-style";
+import { CustomerItem } from "@/services/customer";
+import { EmployeeAssignmentPanel } from "./EmployeeAssignmentPanel";
+import { UserItem } from "@/services/user";
 
 const useStyles = createStyles(({ css, token }) => ({
   customerCard: css`
-    margin-bottom: 24px;
-    padding: 24px;
+    margin-bottom: 18px;
+    border: none;
   `,
   customerInfo: css`
     display: flex;
   `,
   customerAvatar: css`
-    margin-right: 24px;
+    margin-right: 16px;
   `,
-  customerName: css`
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 8px;
-  `,
-  customerMeta: css`
-    color: ${token.colorText};
-    margin-bottom: 8px;
+  customerBaseInfo: css`
+    font-size: 18px;
+    font-weight: 500;
+    margin-bottom: 4px;
+    display: flex;
+    align-items: center;
   `,
   customerNotes: css`
     color: ${token.colorText};
+    font-size: 14px;
+    font-weight: 400;
   `,
-
+  customerMetaContainer: css`
+    display: flex;
+    align-items: center;
+    color: ${token.colorTextTertiary};
+    font-size: 14px;
+    font-weight: 400;
+    margin-bottom: 4px;
+  `,
+  customerMetaSeparator: css`
+    margin: 0 4px;
+  `,
 }));
 
 export interface CustomerInfoCardProps {
@@ -45,29 +55,55 @@ export const CustomerInfoCard: React.FC<CustomerInfoCardProps> = ({
   const { styles } = useStyles();
 
   const { session, extend } = customer;
-
   return (
-    <Card className={styles.customerCard}>
+    <Card
+      className={styles.customerCard}
+      styles={{
+        body: {
+          padding: 0,
+        },
+      }}
+    >
       <Row>
         <Col span={18}>
           <div className={styles.customerInfo}>
             <div className={styles.customerAvatar}>
-              <Avatar size={64} style={{ backgroundColor: '#1890ff' }}>
-                {(session.title || '客户').charAt(0)}
-              </Avatar>
+              {session.avatar ? (
+                <Avatar size={58} src={session.avatar} />
+              ) : (
+                <Avatar>{(session.title || "客户").charAt(0)}</Avatar>
+              )}
             </div>
             <div>
-              <div className={styles.customerName}>
-                {session.title || '未命名客户'}
+              <div className={styles.customerBaseInfo}>
+                <div style={{ marginRight: 8 }}>
+                  {session.title || "未命名客户"}
+                </div>
+                {session?.agent?.title && (
+                  <Tag >{session?.agent?.title}</Tag>
+                )}
+                {extend?.gender && <Tag >{extend.gender}</Tag>}
+                {extend?.age && <Tag >{extend.age + "岁"}</Tag>}
+                {extend?.industry && <Tag >{extend.industry}</Tag>}
+                {extend?.position && <Tag >{extend.position}</Tag>}
               </div>
-              <div className={styles.customerMeta}>
-                创建时间：
-                {session.createdAt
-                  ? new Date(session.createdAt).toLocaleString('zh-CN')
-                  : '-'}
+              <div className={styles.customerMetaContainer}>
+                <div>
+                  创建时间：
+                  {session.createdAt
+                    ? new Date(session.createdAt).toLocaleString("zh-CN")
+                    : "-"}
+                </div>
+                <div className={styles.customerMetaSeparator}>|</div>
+                <div>
+                  更新时间：
+                  {session.updatedAt
+                    ? new Date(session.updatedAt).toLocaleString("zh-CN")
+                    : "-"}
+                </div>
               </div>
               <div className={styles.customerNotes}>
-                备注：{session.description || '-'}
+                备注：{session.description || "-"}
               </div>
             </div>
           </div>
