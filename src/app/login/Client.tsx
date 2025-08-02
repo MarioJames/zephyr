d662c854f22d19f2d82931010b808dbe5d4273ca
Zephyr <38268317+MarioJames@users.noreply.github.com>
2025-08-02 22:42:11 +0800

    feat: 修复打包问题

---
+++ src/app/login/Client.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+export default function Client() {
+  const searchParams = useSearchParams();
+  
+  useEffect(() => {
+    // 获取 callbackUrl 参数，如果没有则默认跳转到首页
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+    
+    // 重定向到 oidc登录页，并传递 callbackUrl
+    signIn('oidc', { 
+      callbackUrl: decodeURIComponent(callbackUrl),
+      redirect: true 
+    });
+  }, [searchParams]);
+
+  return null;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,13 @@
-'use client';
+import BubblesLoading from "@/components/Loading/BubblesLoading";
+import { Suspense } from 'react';
 
-import { signIn } from 'next-auth/react';
-import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import Client from './Client';
 
 export default function Login() {
-  const searchParams = useSearchParams();
-  
-  useEffect(() => {
-    // 获取 callbackUrl 参数，如果没有则默认跳转到首页
-    const callbackUrl = searchParams.get('callbackUrl') || '/';
-    
-    // 重定向到 oidc登录页，并传递 callbackUrl
-    signIn('oidc', { 
-      callbackUrl: decodeURIComponent(callbackUrl),
-      redirect: true 
-    });
-  }, [searchParams]);
 
-  return null;
+  return (
+    <Suspense fallback={BubblesLoading}>
+      <Client />
+    </Suspense>
+  );
 }
