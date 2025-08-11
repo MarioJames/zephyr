dd766b55edd9d3b826379f2fcdc974a68ba30470
Elegy <270663716@qq.com>
2025-08-12 00:10:27 +0800

    feat: 手机号去重网站提示

--- src/app/(main)/customer/form/[[...params]]/features/FormSections/ContactInfoSection.tsx
+++ src/app/(main)/customer/form/[[...params]]/features/FormSections/ContactInfoSection.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Form, Row, Col } from 'antd';
 import { Input } from '@lobehub/ui';
+import { LinkOutlined } from '@ant-design/icons';
 import { useSharedStyles } from '../shared/styles';
+
+// 打开韩国手机号去重查询网站
+const handlePhoneCheckClick = () => {
+  window.open('http://47.81.10.252/', '_blank', 'noopener,noreferrer');
+};
 
 export default function ContactInfoSection() {
   const { styles } = useSharedStyles();
@@ -12,7 +18,27 @@ export default function ContactInfoSection() {
       <Row gutter={24}>
         <Col span={8}>
           <Form.Item
-            label='手机号'
+            label={
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
+                <span>手机号</span>
+                <a
+                  onClick={handlePhoneCheckClick}
+                  style={{ 
+                    color: '#1677ff',
+                    cursor: 'pointer',
+                    fontSize: '14px',
+                    textDecoration: 'none',
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '4px'
+                  }}
+                  title="点击查询手机号去重"
+                >
+                  <LinkOutlined />
+                  <span>手机号去重查询</span>
+                </a>
+              </div>
+            }
             name='phone'
             rules={[
               { required: true, message: '请输入手机号' }
